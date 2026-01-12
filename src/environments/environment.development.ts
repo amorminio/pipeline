8f0b0bffa5298a2df31f4dc43ae5cbe7c812396c
raphael.amorminio <raphael.amorminio@tabcontrol.com.br>
2026-01-11 22:01:16 -0300

    correção environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://economia.awesomeapi.com.br/json/'
+    apiUrl: 'http://localhost:3000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:3000'
+    apiUrl: 'https://economia.awesomeapi.com.br/json/'
+
 };
